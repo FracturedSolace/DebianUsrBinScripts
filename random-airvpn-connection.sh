b0c8cc0061cdad118d5fcfc1947529ca082370b9
root <40284321+FracturedSolace@users.noreply.github.com>
2022-12-13 00:45:50 -0700

    Re-order variables for readability

--- random-airvpn-connection.sh
+++ random-airvpn-connection.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 serverDirectory="/etc/openvpn/server/"
+randomServer="${serverDirectory}$(ls $serverDirectory | sort -R | tail -1)"
+
 # Run script as root if it's run by another user
 if [[ $EUID -ne 0 ]]; then
 	echo "${BASH_SOURCE[0]} must be run as root. Attempting to sudo..."
@@ -7,6 +9,5 @@ if [[ $EUID -ne 0 ]]; then
 	exit
 fi
 
-randomServer="${serverDirectory}$(ls $serverDirectory | sort -R | tail -1)"
 echo "Connecting to random AirVPN Server [${randomServer}]"
 /usr/sbin/openvpn ${randomServer}
