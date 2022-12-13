0f9c5744f0f295113310973bdf2b220c790490ed
root <40284321+FracturedSolace@users.noreply.github.com>
2022-12-13 04:10:03 -0700

    Refactored sudoers file check into a function for ease of readability

--- random-airvpn-connection.sh
+++ random-airvpn-connection.sh
@@ -12,28 +12,30 @@ fi
 
 # Verify that this script can be sudoed by all users regardless of normal permissions
 #	(without password)
-nopasswd_sudo_enabled=false
-regex="^all[[:space:]]+all[[:space:]]*=\(root\)[[:space:]]+(.*:)*nopasswd:(.*:)*[[:space:]]+(.*)"
-# ALL	ALL=(root) 	NOPASSWD:{accept but ignore any add'l paramters} {script name}
-while read line; do
-	if $nopasswd_sudo_enabled; then break; fi
-	
-	[[ ${line,,} =~ $regex ]]
-	if [[ ${#BASH_REMATCH[@]} > 0 ]]; then
-		# Bash regex doesn't allow us to create ignored capture groups, so we need to always check
-		#	the last regex group in the array
-		length=$(( ${#BASH_REMATCH[@]} - 1))
-		capture=${BASH_REMATCH[$length]}
-		# If the captured regex matches our script source,
-		#	that means this script can sudo as intended
-		if [[ ${capture} == ${current_script} ]]; then
-			echo "Found it!"
-			nopasswd_sudo_enabled=true
+nopasswd_sudo_enabled () {
+	regex="^all[[:space:]]+all[[:space:]]*=\(root\)[[:space:]]+(.*:)*nopasswd:(.*:)*[[:space:]]+(.*)"
+	# ALL	ALL=(root) 	NOPASSWD:{accept but ignore any add'l paramters} {script name}
+	while read line; do
+		if $nopasswd_sudo_enabled; then break; fi
+		
+		[[ ${line,,} =~ $regex ]]
+		if [[ ${#BASH_REMATCH[@]} > 0 ]]; then
+			# Bash regex doesn't allow us to create ignored capture groups, so we need to always check
+			#	the last regex group in the array
+			length=$(( ${#BASH_REMATCH[@]} - 1))
+			capture=${BASH_REMATCH[$length]}
+			# If the captured regex matches our script source,
+			#	that means this script can sudo as intended
+			if [[ ${capture} == ${current_script} ]]; then
+				return 1
+			fi
 		fi
-	fi
-done < <(sudo cat /etc/sudoers)
+	done < <(sudo cat /etc/sudoers)
+	
+	return 0
+}
 
-echo "No Passwd Sudo Enabled: ${nopasswd_sudo_enabled}"
+[[ nopasswd_sudo_enabled ]] && echo 'No Passwd Sudo is Enabled' || echo 'No Passwd Sudo is Disabled'
 
 echo "Connecting to random AirVPN Server [$(basename ${randomServer})]"
 /usr/sbin/openvpn ${randomServer} &> /dev/null
